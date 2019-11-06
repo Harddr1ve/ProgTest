4b1f53c2fb9ab16e260389dd71c9cc8a57242e35
Harddrive <kolchinalexandr@gmail.com>
2019-11-07 00:56:46 +0300

    Added menu Class, menu class functions

---
+++ TestProg/TestProg/Menu.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+class Menu
+{
+public:
+	void mainMenu();
+	void createDefaultObject();
+	void createCustomObject();
+	void removeObject();
+
+};
+
--- TestProg/TestProg/main.cpp
+++ TestProg/TestProg/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include "Menu.h"
+
 int main(void)
 {
 	std::cout << "Hello World";
+
+	Menu* menu = new Menu;
 
 	return 0;
 }
